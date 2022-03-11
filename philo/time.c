15b9f7623abbf7cb9e0e4661feb0368bf09beda2
rkrocha <rafaelkr7@gmail.com>
2022-03-11 17:25:12 -0300

    added function time_left_to_live

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 15:25:35 by rkochhan          #+#    #+#             */
-/*   Updated: 2022/03/09 15:40:43 by rkochhan         ###   ########.fr       */
+/*   Updated: 2022/03/11 16:14:56 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ t_time	current_time(void)
 
 	gettimeofday(&time, NULL);
 	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+t_time	time_left_to_live(t_philo *philo)
+{
+	return (time_diff(current_time(), philo->last_meal + philo->time_to_die));
 }
 
 t_time	time_diff(t_time start, t_time end)
